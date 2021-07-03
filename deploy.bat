6f851f0aab0c281b1b48d32d4383beb982a47fb5
码路工人 <maonianyou@foxmail.com>
2021-07-03 08:45:59 +0800

    chore: update deploy

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,11 @@
 cd example/dist
 git init
+git config user.name "码路工人"
+git config user.email maonianyou@gmail.com
 git add -A
 git commit -m deploy 
-git remote add origin https://gitee.com/coder-monkey/watermarker.git
-git pull
-git push --force origin HEAD:watermarker-pages
+git remote add gitee https://github.com/codermonkie/watermark-js.git
+git remote add github git@github.com:CoderMonkie/watermark-js.git
+git pull github
+git pull gitee
+git push --force github HEAD:watermark-pages
