57a9aa0616b952f99b5ddf590413b684142b38e7
码路工人 <maonianyou@foxmail.com>
2021-07-03 17:02:22 +0800

    chore: (deploy) fix gitee url

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,7 @@ git config user.name "码路工人"
 git config user.email maonianyou@gmail.com
 git add -A
 git commit -m deploy 
-git remote add gitee https://github.com/codermonkie/js-watermarker.git
+git remote add gitee https://gitee.com/coder-monkey/js-watermarker.git
 git remote add github git@github.com:CoderMonkie/js-watermarker.git
 git pull github
 git pull gitee
