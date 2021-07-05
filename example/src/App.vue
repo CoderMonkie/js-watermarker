6dd4ff9b029b437e317e18f924ec7becd71881ac
码路工人 <maonianyou@foxmail.com>
2021-07-05 15:01:22 +0800

    doc: update DEMO
    完善 demo

--- example/src/App.vue
+++ example/src/App.vue
@@ -26,6 +26,10 @@
           </el-input>
         </el-form-item>
 
+        <el-divider content-position="left">
+          <h3>imageStyle</h3>
+        </el-divider>
+
         <el-form-item label="是否重复">
           <el-radio-group
             v-model="formData.repeat"
@@ -35,83 +39,6 @@
             <el-radio label="repeat">是</el-radio>
             <el-radio label="no-repeat">否</el-radio>
           </el-radio-group>
-        </el-form-item>
-
-        <el-form-item label="文字排版">
-          <el-radio-group
-            border
-            v-model="formData.textAlign"
-            size="small"
-            @change="handleOptionsChange"
-          >
-            <el-radio label="left">左</el-radio>
-            <el-radio label="right">右</el-radio>
-          </el-radio-group>
-          <span style="margin-left: 50px">
-            <span style="margin-right: 10px">文字定位</span>
-            <span style="display: inline-block; width: 120px">
-              <el-input
-                v-model="formData.textLeft"
-                type="number"
-                :min="0"
-                :input-style="{ textAlign: 'right' }"
-                @change="handleOptionsChange"
-              >
-                <template v-slot:prefix> Left: </template>
-              </el-input>
-            </span>
-            <span style="display: inline-block; width: 120px">
-              <el-input
-                v-model="formData.textTop"
-                type="number"
-                :min="0"
-                :input-style="{ textAlign: 'right' }"
-                @change="handleOptionsChange"
-              >
-                <template v-slot:prefix> Top: </template>
-              </el-input>
-            </span>
-          </span>
-          <div></div>
-        </el-form-item>
-
-        <el-form-item label="字体大小">
-          <el-input-number
-            v-model="formData.fontSize"
-            :min="10"
-            :max="48"
-            :step="1"
-            step-strictly
-            :controls="false"
-            @change="handleOptionsChange"
-          ></el-input-number>
-          <span class="font-size-tip">10&nbsp;~&nbsp;48&nbsp;step 1</span>
-        </el-form-item>
-
-        <el-form-item label="多行文本时用于累加的行高">
-          <el-input-number
-            v-model="formData.lineHeight"
-            :min="15"
-            :max="60"
-            :step="1"
-            step-strictly
-            :controls="false"
-            @change="handleOptionsChange"
-          ></el-input-number>
-          <span class="font-size-tip">15&nbsp;~&nbsp;60&nbsp;step 1</span>
-        </el-form-item>
-
-        <el-form-item label="文字倾斜">
-          <el-input-number
-            v-model="formData.rotate"
-            :min="-360"
-            :max="360"
-            :step="5"
-            step-strictly
-            :controls="false"
-            @change="handleOptionsChange"
-          ></el-input-number>
-          <span class="rotate-tip">-360&nbsp;~&nbsp;360&nbsp;step 10</span>
         </el-form-item>
 
         <el-form-item label="水印位置">
@@ -165,10 +92,126 @@
             </el-input>
           </span>
         </el-form-item>
+
+        <el-divider content-position="left">
+          <h3>textStyle</h3>
+        </el-divider>
+
+        <el-form-item label="文字排版">
+          <el-radio-group
+            border
+            v-model="formData.textAlign"
+            size="small"
+            @change="handleOptionsChange"
+          >
+            <el-radio label="left">左</el-radio>
+            <el-radio label="right">右</el-radio>
+          </el-radio-group>
+          <span style="margin-left: 50px">
+            <span style="margin-right: 10px">文字定位</span>
+            <span style="display: inline-block; width: 120px">
+              <el-input
+                v-model="formData.textLeft"
+                type="number"
+                :min="0"
+                :input-style="{ textAlign: 'right' }"
+                @change="handleOptionsChange"
+              >
+                <template v-slot:prefix> Left: </template>
+              </el-input>
+            </span>
+            <span style="display: inline-block; width: 120px">
+              <el-input
+                v-model="formData.textTop"
+                type="number"
+                :min="0"
+                :input-style="{ textAlign: 'right' }"
+                @change="handleOptionsChange"
+              >
+                <template v-slot:prefix> Top: </template>
+              </el-input>
+            </span>
+          </span>
+          <div></div>
+        </el-form-item>
+
+        <el-form-item label="字体大小">
+          <el-input-number
+            v-model="formData.fontSize"
+            :min="10"
+            :max="48"
+            :step="1"
+            step-strictly
+            :controls="false"
+            @change="handleOptionsChange"
+          ></el-input-number>
+          <span class="form-item__tip font-size-tip"
+            >10&nbsp;~&nbsp;48&nbsp;(step 1)</span
+          >
+        </el-form-item>
+
+        <el-form-item label="多行文本时用于累加的行高">
+          <el-input-number
+            v-model="formData.lineHeight"
+            :min="15"
+            :max="60"
+            :step="1"
+            step-strictly
+            :controls="false"
+            @change="handleOptionsChange"
+          ></el-input-number>
+          <span class="form-item__tip font-size-tip"
+            >15&nbsp;~&nbsp;60&nbsp;(step 1)</span
+          >
+        </el-form-item>
+
+        <el-form-item label="文字倾斜">
+          <el-input-number
+            v-model="formData.rotate"
+            :min="-360"
+            :max="360"
+            :step="5"
+            step-strictly
+            :controls="false"
+            @change="handleOptionsChange"
+          ></el-input-number>
+          <span class="form-item__tip rotate-tip"
+            >-360&nbsp;~&nbsp;360&nbsp;(step 5)</span
+          >
+        </el-form-item>
+
+        <el-form-item label="颜色">
+          <el-color-picker
+            v-model="formData.textColor"
+            show-alpha
+            color-format="hex"
+            @change="handleOptionsChange"
+          ></el-color-picker>
+        </el-form-item>
+        <el-form-item label="透明度">
+          <el-input-number
+            v-model="formData.textOpacity"
+            :min="0.01"
+            :max="1"
+            :step="0.01"
+            step-strictly
+            :controls="false"
+            @change="handleOptionsChange"
+          ></el-input-number>
+          <span class="form-item__tip text-opacity-tip"
+            >0.01&nbsp;~&nbsp;1&nbsp;(step 0.01)</span
+          >
+          <span class="form-item__tip text-opacity-tip">
+            &gt;&gt;&nbsp;当颜色中使用了透明度(即
+            RGBA)时，此处的透明度会同时起作用，建议使用默认值 1</span
+          >
+        </el-form-item>
       </el-form>
     </section>
 
     <section class="page-section__setting-json">
+      生成的配置对象：
+      <hr />
       <pre
         >{{ JSON.stringify(computedOptions, null, 4) }}
       </pre>
@@ -203,6 +246,8 @@ export default {
         fontSize: 16,
         lineHeight: 25,
         rotate: -10,
+        textColor: "#fee0b9",
+        textOpacity: 1,
         // imageStyle
         repeat: "repeat",
         imagePositionX: "left",
@@ -226,10 +271,11 @@ export default {
             left: parseInt(state.formData.textLeft),
             top: parseInt(state.formData.textTop),
             rotate: state.formData.rotate,
-            color: "#fee0b9",
             align: state.formData.textAlign,
             fontSize: state.formData.fontSize,
             lineHeight: state.formData.lineHeight,
+            color: state.formData.textColor,
+            alpha: state.formData.textOpacity,
           },
           imageStyle: {
             /** 水印图片/canvas 的尺寸大小 */
@@ -282,8 +328,7 @@ export default {
   }
 
   .page-section__settings {
-    .font-size-tip,
-    .rotate-tip {
+    .form-item__tip {
       margin-left: 10px;
       color: #999999;
     }
